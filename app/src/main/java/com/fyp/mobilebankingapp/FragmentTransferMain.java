7f40b07392229cc740624b8107989e4aa1553560
lkchong <lkchong91@gmail.com>
2018-09-17 22:01:02 +0800

    -linked transfer main page to "transfer to own account" and "transfer to other account" page.
    -TODO: retrieve customer's bank account list from DB.

--- app/src/main/java/com/fyp/mobilebankingapp/FragmentTransferMain.java
+++ app/src/main/java/com/fyp/mobilebankingapp/FragmentTransferMain.java
@@ -1,6 +1,7 @@
 package com.fyp.mobilebankingapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,7 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class FragmentTransferMain extends Fragment {
@@ -21,7 +22,25 @@ public class FragmentTransferMain extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_transfer_main, container, false);
+
+        Button buttonOwn = view.findViewById(R.id.transferToOwnAccBtn);
+        buttonOwn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TransferToOwnAcc.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonOther = view.findViewById(R.id.transferToOtherAccBtn);
+        buttonOther.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), TransferToOtherAcc.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
-
 }
